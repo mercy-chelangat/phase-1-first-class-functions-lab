19aaca31eaf9e20a284addc94f1496471ae578d2
mercy chelangat <chelangatmercy194@gmail.com>
2024-03-22 17:11:41 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+// Function to return the first two drivers from an array
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  }
+  
+  // Function to return the last two drivers from an array
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  }
+  
+  // Array containing the two driver selection functions
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  // Higher-order function to create a fare multiplier
+  const createFareMultiplier = function(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    }
+  }
+  
+  // Function to double the fare
+  const fareDoubler = createFareMultiplier(2);
+  
+  // Function to triple the fare
+  const fareTripler = createFareMultiplier(3);
+  
+  // Function to select different drivers based on the provided selection function
+  const selectDifferentDrivers = function(drivers, selectionFunction) {
+    return selectionFunction(drivers);
+  }
+  
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,40 +1,4 @@
-require ( './helpers.js' );
-
-describe('index.js', function () {
-  const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
-
-  afterEach(function () {
-    expect(drivers, 'MAKE SURE YOUR ARRAY MANIPULATIONS ARE NON-DESTRUCTIVE').to.eql(['Antonia', 'Nuru', 'Amari', 'Mo']);
-  });
-
-  describe('returnFirstTwoDrivers()', function () {
-    it('should return a new array containing the first two drivers from the passed-in array', function () {
-      expect(returnFirstTwoDrivers(drivers)).to.eql(['Antonia', 'Nuru']);
-    });
-  });
-
-  describe('returnLastTwoDrivers()', function () {
-    it('should return an array of the last two drivers', function () {
-      expect(returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])).to.eql(['Amari', 'Mo']);
-    });
-  });
-
-  describe('selectingDrivers', function () {
-    it('has the `returnFirstTwoDrivers` function to as its first element', function () {
-      expect(selectingDrivers[0]).to.eql(returnFirstTwoDrivers);
-    });
-
-    it('has the `returnLastTwoDrivers` function to as its last element', function () {
-      expect(selectingDrivers[1]).to.eql(returnLastTwoDrivers);
-    });
-
-    it('allows us to invoke either function from the array', function () {
-      expect(selectingDrivers[0](drivers)).to.eql(['Antonia', 'Nuru']);
-
-      expect(selectingDrivers[1](drivers)).to.eql(['Amari', 'Mo']);
-    });
-  });
-
+rC
   describe('createFareMultiplier()', function () {
     it('returns a function', function () {
       const fareMultiplier = createFareMultiplier(2);
