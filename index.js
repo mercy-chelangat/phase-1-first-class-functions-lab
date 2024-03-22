90a93769e31401de8c4cc936b4c0120b2f4192b6
mercy chelangat <chelangatmercy194@gmail.com>
2024-03-22 18:45:25 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 // Code your solution in this file!
-// Function to return the first two drivers from an array
-const returnFirstTwoDrivers = function(drivers) {
-    return drivers.slice(0, 2);
-  }
+// Declare a variable in global scope called customerName using the var keyword and assign it the value 'bob'.
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+    let bestCustomer = 'not bob';
+
+ 
+}
+function overwriteBestCustomer() {
+ let BestCustomer = 'newBestCustome'
+}
+let leastFavoriteCustomer = 'someone'
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'someone else';
   
-  // Function to return the last two drivers from an array
-  const returnLastTwoDrivers = function(drivers) {
-    return drivers.slice(-2);
-  }
-  
-  // Array containing the two driver selection functions
-  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
-  
-  // Higher-order function to create a fare multiplier
-  const createFareMultiplier = function(multiplier) {
-    return function(fare) {
-      return fare * multiplier;
-    }
-  }
-  
-  // Function to double the fare
-  const fareDoubler = createFareMultiplier(2);
-  
-  // Function to triple the fare
-  const fareTripler = createFareMultiplier(3);
-  
-  // Function to select different drivers based on the provided selection function
-  const selectDifferentDrivers = function(drivers, selectionFunction) {
-    return selectionFunction(drivers);
-  }
-  
+let leastFavoriteCustomer = 'initial value'; 
+leastFavoriteCustomer = 'someone else';
+}
+
+module.exports = {
+  customerName,
+  upperCaseCustomerName,
+  setBestCustomer,
+  overwriteBestCustomer,
+  changeLeastFavoriteCustomer
+};
+
